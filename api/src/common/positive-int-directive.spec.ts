713dbef275fe10cd46f630637f6232ffa63d24da
Rik Kierkels <kierkels.r@gmail.com>
2020-12-19 13:04:42 +0100

    Add additional tests for the positive int directive

--- api/src/common/positive-int-directive.spec.ts
+++ api/src/common/positive-int-directive.spec.ts
@@ -1,4 +1,4 @@
-import { ApolloServer, gql, makeExecutableSchema, SchemaDirectiveVisitor } from 'apollo-server';
+import { ApolloServer, gql, makeExecutableSchema, SchemaDirectiveVisitor, UserInputError } from 'apollo-server';
 import { ApolloServerTestClient, createTestClient } from 'apollo-server-testing';
 import { DocumentNode } from 'graphql';
 import { isPositiveIntDirective } from './positive-int-directive';
@@ -6,7 +6,7 @@ import { isPositiveIntDirective } from './positive-int-directive';
 const createApolloTestClient = (
   typeDefs: DocumentNode,
   schemaDirectives: Record<string, typeof SchemaDirectiveVisitor>,
-) => (): ApolloServerTestClient => {
+): ApolloServerTestClient => {
   const server = new ApolloServer({ schema: makeExecutableSchema({ typeDefs, schemaDirectives }) });
   return createTestClient(server as any);
 };
@@ -15,17 +15,17 @@ test('when applied to a non-scalar type, throws an error', () => {
   const typeDefs = gql`
     directive @isPositive on INPUT_FIELD_DEFINITION
 
-    input Author {
+    input AuthorInput {
       name: String
     }
 
-    input Book {
-      author: Author @isPositive
+    input BookInput {
+      author: AuthorInput @isPositive
     }
   `;
 
-  expect(createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective })).toThrowError(
-    'isPositive directive was applied to a non-scalar type: Author',
+  expect(() => createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective })).toThrowError(
+    'isPositive directive was applied to a non-scalar type: AuthorInput',
   );
 });
 
@@ -33,24 +33,85 @@ test('when applied to an invalid scalar type, throws an error', () => {
   const typeDefs = gql`
     directive @isPositive on INPUT_FIELD_DEFINITION
 
-    input Author {
-      name: String @isPositive
+    input BookInput {
+      title: String @isPositive
     }
   `;
 
-  expect(createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective })).toThrowError(
+  expect(() => createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective })).toThrowError(
     'isPositive directive cannot be applied to scalar of type String',
   );
 });
 
-describe('for a nullable type', () => {
-  it('with a negative integer, throws an error', () => {});
+describe('when applied to an int scalar type', () => {
+  const typeDefs = gql`
+    directive @isPositive on INPUT_FIELD_DEFINITION
 
-  it('with a positive integer, does not throw an error', () => {});
-});
+    type Query {
+      book: Book
+    }
 
-describe('for a non nullable type', () => {
-  it('with a negative integer, throws an error', () => {});
+    type Mutation {
+      createBook(input: BookInput): Book
+    }
 
-  it('with a positive integer, does not throw an error', () => {});
+    input BookInput {
+      pages: Int! @isPositive
+      chapters: Int @isPositive
+    }
+
+    type Book {
+      pages: Int
+    }
+  `;
+
+  const createBookMutation = gql`
+    mutation createBook($input: BookInput) {
+      createBook(input: $input) {
+        pages
+      }
+    }
+  `;
+
+  it('that is non nullable, with a negative integer, throws an error', async () => {
+    const { mutate } = createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective });
+
+    const { errors } = await mutate({ mutation: createBookMutation, variables: { input: { pages: -10 } } });
+
+    expect(errors).toHaveLength(1);
+    expect(errors?.[0].message).toContain('pages must be a positive integer');
+    expect(errors?.[0].extensions).toEqual({ code: 'BAD_USER_INPUT' });
+  });
+
+  it('that is non nullable, with a positive integer, does not throw an error', async () => {
+    const { mutate } = createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective });
+
+    const { errors } = await mutate({ mutation: createBookMutation, variables: { input: { pages: 10 } } });
+
+    expect(errors).toBeUndefined();
+  });
+
+  it('with a negative integer, throws an error', async () => {
+    const { mutate } = createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective });
+
+    const { errors } = await mutate({
+      mutation: createBookMutation,
+      variables: { input: { pages: 10, chapters: 0 } },
+    });
+
+    expect(errors).toHaveLength(1);
+    expect(errors?.[0].message).toContain('chapters must be a positive integer');
+    expect(errors?.[0].extensions).toEqual({ code: 'BAD_USER_INPUT' });
+  });
+
+  it('with a positive integer, does not throw an error', async () => {
+    const { mutate } = createApolloTestClient(typeDefs, { isPositive: isPositiveIntDirective });
+
+    const { errors } = await mutate({
+      mutation: createBookMutation,
+      variables: { input: { pages: 10, chapters: 10 } },
+    });
+
+    expect(errors).toBeUndefined();
+  });
 });
